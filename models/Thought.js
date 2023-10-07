270c3382f906e4b276028453cc64155223b950d6
ijordan4 <isaiahjordan200@gmail.com>
2023-10-06 22:30:32 -0400

    updated models  to fix overall structure as well as routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,68 +1,40 @@
-const {Schema, model, Types} = require('mongoose');
-const moment = require('moment')
+const mongoose = require('mongoose');
 
-const reactionSchema = new Schema (
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 300,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtValue => moment(createdAtValue).format("MMM DD, YYYY [at] hh:mm a"),
-        },
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
+const reactionSchema = new mongoose.Schema({
+  reactionId: mongoose.Schema.Types.ObjectId,
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const thoughtSchema = new Schema (
-    {
-        thoughtText: {
-            type: String,
-            required: true, 
-            minlength: 1,
-            maxlength: 300,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtValue => moment(createdAtValue).format("MMM DD, YYYY [at] hh:mm a"),
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
 
-thoughtSchema.virtual('reactionCount').get(function() {
-        return this.reactions.length;
-    });
-
-const Thought = model('Thought', thoughtSchema);
+const Thought = mongoose.model('Thought', thoughtSchema);
 
 module.exports = Thought;
---
+++ routes/api/api.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../controller/user-controller');
+const {
+  getAllThoughts,
+  createThought,
+  getThoughtById,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../controller/thought-controller');
+
+// User routes
+router.get('/users', getAllUsers);
+router.post('/users', createUser);
+router.get('/users/:userId', getUserById);
+router.put('/users/:userId', updateUser);
+router.delete('/users/:userId', deleteUser);
+router.post('/users/:userId/friends/:friendId', addFriend);
+router.delete('/users/:userId/friends/:friendId', removeFriend);
+
+// Thought routes
+router.get('/thoughts', getAllThoughts);
+router.post('/thoughts', createThought);
+router.get('/thoughts/:thoughtId', getThoughtById);
+router.put('/thoughts/:thoughtId', updateThought);
+router.delete('/thoughts/:thoughtId', deleteThought);
+router.post('/thoughts/:thoughtId/reactions', createReaction);
+router.delete('/thoughts/:thoughtId/reactions', deleteReaction);
+
+module.exports = router;
